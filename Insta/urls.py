2a3d27c413138623414f158305a7640203db14d3
leftychen <lefty-chen@hotmail.com>
2020-05-07 14:26:30 -0400

    Check in pic list

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView
 
 urlpatterns = [
-    path('',HelloWorld.as_view(), name='helloworld')
+    path('',HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail')
 ]
